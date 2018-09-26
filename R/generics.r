6d5d501780bebdc0d22726e2f7556e98837f69b2
bautheac <o.bautheac@gmail.com>
2018-09-26 22:25:55 +0200

    Move plotit to finRes

--- R/generics.r
+++ R/generics.r
@@ -416,3 +416,106 @@ setGeneric("get_periods", function(object) standardGeneric("get_periods"))
 #'
 #' @export
 setGeneric("summary", function(object, leg = "factor") standardGeneric("summary"))
+
+
+
+
+
+
+# plot ####
+
+#' Plots futures term structure over time.
+#'
+#' @description Plots historical futures term structure data contained in an S4 object
+#'   of class \linkS4class{FuturesTS} from the \href{https://github.com/bautheac/pullit/}{\pkg{pullit}} package.
+#'
+#' @param object an S4 object of class \linkS4class{FuturesTS}.
+#' @param ticker a scalar character vector. Active contract Bloomberg ticker to plot the term structure for.
+#' @param frame a scalar integer vector. Animation speed parameter; the lower the faster.
+#'
+#' @docType methods
+#' @rdname plot_term_structure-methods
+#'
+#' @export
+setGeneric("plot_term_structure", function(object, ticker, frame) standardGeneric("plot_term_structure"))
+
+
+
+#' Plots market performance for various S4 objects ouput of packages that belong to the
+#'   \href{https://bautheac.github.io/finRes/}{\pkg{finRes}} suite.
+#'
+#' @description Plots historical market performance inficators for various S4 objects
+#'   from packages belonging to the \href{https://bautheac.github.io/finRes/}{\pkg{finRes}} suite.
+#'
+#' @param object an S4 object of class \linkS4class{DataHistorical} and childs or \linkS4class{AssetPricingFactor}.
+#' @param ... extra parameters for \code{object} related methods.
+#'
+#' @examples \dontrun{
+#'   library(finRes)
+#'
+#'   # funds ####
+#'   ## pull data from Bloomberg with pullit ####
+#'   fund <- BBG_fund_market(tickers = "SPY US Equity",
+#'     start = as.character(Sys.Date() - 365L), end = as.character(Sys.Date()))
+#'   ## plot fund performance ####
+#'   plot_performance(fund, ticker = "SPY US Equity")
+#'
+#'   # asset pricing factors ####
+#'   ## pull data from Bloomberg via pullit ####
+#'   term_structure <- BBG_futures_market(type = 'term structure',
+#'     active_contract_tickers = c("C A Comdty", "S A Comdty", "SMA Comdty", "BOA Comdty",
+#'       "W A Comdty", "KWA Comdty", "MWA Comdty", "O A Comdty",
+#'       "CHEA Comdty", "V6A Comdty", "FCA Comdty", "LCA Comdty", "LHA Comdty",
+#'       "SBA Comdty", "CCA Comdty", "CTA Comdty", "KCA Comdty", "JOA Comdty", "LBA Comdty",
+#'       "GCA Comdty", "SIA Comdty", "PAA Comdty", "PLA Comdty", "LAA Comdty", "LLA Comdty",
+#'       "LNA Comdty", "LPA Comdty", "LTA Comdty", "LXA Comdty",
+#'       "CLA Comdty", "COA Comdty", "DLA Comdty", "NGA Comdty", "HOA Comdty", "XBA Comdty"),
+#'     start = as.character(Sys.Date() - (2L * 365L)), end = as.character(Sys.Date()),
+#'     TS_positions = 1L, roll_type = "A", roll_days = 0L, roll_months = 0L, roll_adjustment = "N")
+#'   ## construct an asset pricing factor with factorem ####
+#'   factor <- momentum_factor(term_structure)
+#'   ## plot factor performance ####
+#'   plot_performance(factor)
+#' }
+#'
+#'
+#' @docType methods
+#' @rdname plot_performance-methods
+#'
+#' @export
+setGeneric("plot_performance", function(object, ...) standardGeneric("plot_performance"))
+
+
+
+#' Plots factor positions summary by legs
+#'
+#' @description Plots a summary of a factor's positions by legs.
+#'
+#' @param object an S4 object of class \linkS4class{AssetPricingFactor}.
+#'
+#'
+#' @examples \dontrun{
+#'   library(finRes)
+#'   term_structure <- BBG_futures_market(type = 'term structure',
+#'     active_contract_tickers = c("C A Comdty", "S A Comdty", "SMA Comdty", "BOA Comdty",
+#'       "W A Comdty", "KWA Comdty", "MWA Comdty", "O A Comdty",
+#'       "CHEA Comdty", "V6A Comdty", "FCA Comdty", "LCA Comdty", "LHA Comdty",
+#'       "SBA Comdty", "CCA Comdty", "CTA Comdty", "KCA Comdty", "JOA Comdty", "LBA Comdty",
+#'       "GCA Comdty", "SIA Comdty", "PAA Comdty", "PLA Comdty", "LAA Comdty", "LLA Comdty",
+#'       "LNA Comdty", "LPA Comdty", "LTA Comdty", "LXA Comdty",
+#'       "CLA Comdty", "COA Comdty", "DLA Comdty", "NGA Comdty", "HOA Comdty", "XBA Comdty"),
+#'     start = as.character(Sys.Date() - (2L * 365L)), end = as.character(Sys.Date()),
+#'     TS_positions = 1L, roll_type = "A", roll_days = 0L, roll_months = 0L, roll_adjustment = "N")
+#'   factor <- momentum_factor(term_structure)
+#'   plot_positions(factor)
+#' }
+#'
+#'
+#' @docType methods
+#' @rdname plot_positions-methods
+#'
+#'
+#' @export
+setGeneric("plot_positions", function(object) standardGeneric("plot_positions"))
+
+
--- development/development.r
+++ development/development.r
@@ -1,3 +1,13 @@
 options(usethis.full_name = "Olivier Bauthéac")
 usethis::use_gpl3_license()
 usethis::use_readme_md()
+
+
+test <- readRDS(file = "O:/Dropbox/Code/R/Packages/storethat/development/data - 2018-09-22.rds")
+
+plot_performance(object = test$data[[7L]], ticker = "IVV US Equity")
+
+test$data[[7L]]@tickers
+?plot_performance
+
+
